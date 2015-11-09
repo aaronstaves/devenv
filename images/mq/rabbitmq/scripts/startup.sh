0946681e2439cbdb2b0ccb87b67e01443d6e928e
JOHN WALSTRA <jwalstra@macbook.local>
2015-11-09 08:11:18 -0600

    Update

--- images/mq/rabbitmq/scripts/startup.sh
+++ images/mq/rabbitmq/scripts/startup.sh
@@ -6,18 +6,14 @@ export CONFIG_FILE=$RABBITMQ_CONFIG_FILE
 export RABBITMQ_LOG_BASE=/var/log/mq/rabbitmq
 export LOG_BASE=$RABBITMQ_LOG_BASE
 
-if [ ! -f /etc/.installed ]; then
+echo "CONFIG_FILE=$CONFIG_FILE" >  /etc/rabbitmq/rabbitmq-env.conf
+echo "LOG_BASE=$LOG_BASE"       >> /etc/rabbitmq/rabbitmq-env.conf
 
-	echo "CONFIG_FILE=$CONFIG_FILE" >  /etc/rabbitmq/rabbitmq-env.conf
-	echo "LOG_BASE=$LOG_BASE"       >> /etc/rabbitmq/rabbitmq-env.conf
+mkdir -p /data/etc/mq/rabbitmq
+cp /tmp/rabbitmq.config /data/etc/mq/rabbitmq/rabbitmq.config
 
-
-	mkdir -p /data/etc/mq/rabbitmq
-	cp /tmp/rabbitmq.config /data/etc/mq/rabbitmq/rabbitmq.config
-
-	rm  /etc/rabbitmq/rabbitmq.config
-	ln -sf /data/etc/mq/rabbitmq/rabbitmq.config  /etc/rabbitmq/rabbitmq.config
-fi
+rm -f /etc/rabbitmq/rabbitmq.config
+ln -sf /data/etc/mq/rabbitmq/rabbitmq.config  /etc/rabbitmq/rabbitmq.config
 
 if [ ! -d /data/var/mq/rabbitmq ]; then
 
